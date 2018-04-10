930f9919bf0021993cbb8b4793c75d91412cf45b
sbyTechpoint <soufiane.benyoussef@gmail.com>
2018-04-10 11:41:03 +0200

    Update Collection.types.ts

--- src/Collections/Generic/Collection.types.ts
+++ src/Collections/Generic/Collection.types.ts
@@ -1,7 +1,10 @@
+
 export interface IEqualityComparer < T > {
+    // callBack declaration
     (a : T, b : T): boolean;
 }
 
+//Interface for ICollection
 export interface ICollection < T > {
     add(item : T): this;
     count(): number;
@@ -12,12 +15,15 @@ export interface ICollection < T > {
     remove(item : T): this;
 }
 
+//interface for IList
 export interface IList < T > extends ICollection < T > {
     addRange(...items : T[]): this;
     indexOf(item : T, compareFn?: IEqualityComparer < T >): number;
     insert(index : number, item : T): this;
     removeAt(index : number): this;
 }
+
 export interface IGroupedBy < T > {
+    //key index declaration
     [key : string]: T[];
 }
